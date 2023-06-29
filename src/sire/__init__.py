d2c138b0b3be33392f03f8f512df683ba571ff1a
Christopher Woods <chryswoods@hey.com>
2023-06-29 18:28:39 +0100

    Committing everything ready for the PR.
    Updated things in the changelog.
    
    Sped up the code a lot, added tests, added docs.

--- src/sire/__init__.py
+++ src/sire/__init__.py
@@ -16,7 +16,7 @@ from ._load import (
     smarts,
 )
 
-from ._measure import measure
+from ._measure import measure, minimum_distance
 from ._colname import colname, colnames
 from ._parallel import (
     get_max_num_threads,
@@ -33,6 +33,7 @@ __all__ = [
     "get_max_num_threads",
     "load",
     "load_test_files",
+    "minimum_distance",
     "measure",
     "molid",
     "save",
