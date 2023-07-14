8e6426dfd5c732c5f81e708d1923ed6f3beceeb9
Christopher Woods <chryswoods@hey.com>
2023-07-14 17:39:21 +0100

    Added a test that checks that the velocities are correct.
    [ci skip]

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,7 +43,9 @@ def pytest_collection_modifyitems(config, items):
                 item.add_marker(skip_slow)
 
     if not runveryslow:
-        skip_veryslow = pytest.mark.skip(reason="need --runveryslow option to run")
+        skip_veryslow = pytest.mark.skip(
+            reason="need --runveryslow option to run"
+        )
         for item in items:
             if "veryslow" in item.keywords:
                 item.add_marker(skip_veryslow)
@@ -106,4 +108,20 @@ def wrapped_mols():
 
 @pytest.fixture(scope="session")
 def openmm_interchange_mols():
-    return sr.load_test_files("openmm_interchange.rst7", "openmm_interchange.prm7")
+    return sr.load_test_files(
+        "openmm_interchange.rst7", "openmm_interchange.prm7"
+    )
+
+
+@pytest.fixture(scope="session")
+def triclinic_protein():
+    return sr.load_test_files(
+        "triclinic_protein.prm7", "triclinic_protein.crd"
+    )
+
+
+@pytest.fixture(scope="session")
+def triclinic_protein_rst7():
+    return sr.load_test_files(
+        "triclinic_protein.prm7", "triclinic_protein.rst"
+    )
