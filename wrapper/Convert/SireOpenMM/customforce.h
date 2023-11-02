9ba807cfcb777e278b2ec500a6c7d0342c9adc45
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2023-11-02 00:35:57 +0000

    Beginning to think about what data is needed for GridForce

--- wrapper/Convert/SireOpenMM/customforce.h
+++ wrapper/Convert/SireOpenMM/customforce.h
@@ -1,6 +1,7 @@
 #ifndef SIRE_OPENMM_CUSTOMFORCE_H
 #define SIRE_OPENMM_CUSTOMFORCE_H
 
+#include "openmm.h"
 #include "openmm/Force.h"
 
 #include "sireglobal.h"
@@ -15,8 +16,40 @@ namespace SireOpenMM
         GridForce();
         ~GridForce();
 
+        int addFixedAtom(const OpenMM::Vec3 &position,
+                         float charge, float sigma, float epsilon);
+
+        void getFixedAtom(int idx, OpenMM::Vec3 &position,
+                          float &charge, float &sigma, float &epsilon) const;
+
+        void updateFixedAtom(int idx, const OpenMM::Vec3 &position,
+                             float charge, float sigma, float epsilon);
+
     protected:
         OpenMM::ForceImpl *createImpl() const;
+
+    private:
+        /** All of the data for the fixed atoms */
+        std::vector<std::tuple<OpenMM::Vec3, float, float, float>> fixed_atoms;
+
+        /** The coulomb potential grid */
+        std::vector<double> coulomb_grid;
+
+        /** The center of the grid */
+        OpenMM::Vec3 grid_center;
+
+        /** The half-extents of this grid (plus or minus this to
+            get the grid boundaries) */
+        OpenMM::Vec3 grid_half_extents;
+
+        /** The coulomb cutoff (applies from the center of the grid) */
+        double coulomb_cutoff;
+
+        /** The grid spacing */
+        double grid_spacing;
+
+        /** The number of grid points along x, y and z */
+        unsigned int dimx, dimy, dimz;
     };
 }
 
