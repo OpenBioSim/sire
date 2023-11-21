7b5b24651f775c4d7f0b74cbc6ec2dd9e38043fc
Lester Hedges <lester.hedges@gmail.com>
2023-11-21 11:57:14 +0000

    Add convenience functions for setting/unsetting QM molecules.

--- src/sire/system/_system.py
+++ src/sire/system/_system.py
@@ -869,6 +869,56 @@ class System:
 
         self._system.set_property(traj_propname.source(), trajectory)
 
+    def set_qm_molecule(self, index, map=None):
+        """
+        Set the specified molecule to be a QM molecule.
+        """
+        if index < 0 or index > self.num_molecules()-1:
+            raise ValueError(
+                f"Index {index} is out of range for the number of "
+                f"molecules in this system ({self.num_molecules()})"
+            )
+
+        # Check for existing QM molecules. Currently we only support a single
+        # molecule.
+        try:
+            qm_mols = self["property is_qm"].molecules()
+        except:
+            qm_mols = []
+
+        if len(qm_mols) > 0:
+            raise ValueError(
+                "This system already contains a QM molecule!"
+            )
+
+        # Extract the molecule.
+        mol = self[index]
+
+        # Create a cursor.
+        c = mol.cursor()
+
+        # Flag the molecule as QM.
+        c["is_qm"] = True
+
+        # Commit the changes and update.
+        mol = c.commit()
+        self.update(mol)
+
+    def unset_qm_molecule(self, map=None):
+        """
+        Unset the QM molecule.
+        """
+        try:
+            # Loop over the QM molecules and unset them.
+            qm_mols = self["property is_qm"].molecules()
+            for mol in qm_mols:
+                c = mol.cursor()
+                c["is_qm"] = False
+                mol = c.commit()
+                self.update(mol)
+        except:
+            pass
+
     def clear_energy_trajectory(self, map=None):
         """
         Completely clear any existing energy trajectory
