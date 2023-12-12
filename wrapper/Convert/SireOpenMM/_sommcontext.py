a1c5165e153db5944d08431f7213c90421321b1e
Christopher Woods <chryswoods@hey.com>
2023-12-12 17:47:39 +0000

    Make sure to return `None` not `none` if the constraint hasn't been set
    [ci skip]

--- wrapper/Convert/SireOpenMM/_sommcontext.py
+++ wrapper/Convert/SireOpenMM/_sommcontext.py
@@ -66,9 +66,7 @@ class SOMMContext(_Context):
             # place the coordinates and velocities into the context
             _set_openmm_coordinates_and_velocities(self, metadata)
 
-            self._lambda_value = self._lambda_lever.set_lambda(
-                self, lambda_value
-            )
+            self._lambda_value = self._lambda_lever.set_lambda(self, lambda_value)
 
             self._map = map
         else:
@@ -98,7 +96,7 @@ class SOMMContext(_Context):
         if self._map.specified("constraint"):
             return self._map["constraint"].source()
         else:
-            return "none"
+            return None
 
     def get_perturbable_constraint(self):
         """
@@ -107,7 +105,7 @@ class SOMMContext(_Context):
         if self._map.specified("perturbable_constraint"):
             return self._map["perturbable_constraint"].source()
         else:
-            return "none"
+            return None
 
     def get_platform(self):
         """
@@ -256,10 +254,7 @@ class SOMMContext(_Context):
             import openmm
             from ...units import kcal_per_mol
 
-            return (
-                nrg.value_in_unit(openmm.unit.kilocalorie_per_mole)
-                * kcal_per_mol
-            )
+            return nrg.value_in_unit(openmm.unit.kilocalorie_per_mole) * kcal_per_mol
         else:
             return nrg
 
