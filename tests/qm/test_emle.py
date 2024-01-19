651d949b0b9cd4eb74b20131d1fcee98d9e1dd21
Lester Hedges <lester.hedges@gmail.com>
2024-01-19 12:47:19 +0000

    Add test for link atom detection.

--- tests/qm/test_emle.py
+++ tests/qm/test_emle.py
@@ -40,6 +40,62 @@ def test_callback():
     assert result == (42, d, c) == test.callback(a, b, c, d)
 
 
+@pytest.mark.parametrize(
+    "selection, expected",
+    [
+        (
+            "residx 0",
+            (
+                {6: 4},
+                {6: [7, 8]},
+                {6: 0.8164794007490638},
+            ),
+        ),
+        (
+            "residx 1",
+            (
+                {4: 6, 16: 14},
+                {4: [1, 5], 16: [17, 18]},
+                {4: 0.7565543071161049, 16: 0.8164794007490638},
+            ),
+        ),
+        (
+            "residx 2",
+            (
+                {14: 16},
+                {14: [8, 15]},
+                {14: 0.7565543071161049},
+            ),
+        ),
+    ],
+)
+def test_link_atoms(ala_mols, selection, expected):
+    """
+    Make sure that the link atoms are correctly identified.
+    """
+
+    from sire.base import create_map as _create_map
+    from sire.qm._utils import _create_qm_mol_to_atoms, _get_link_atoms
+
+    # Create a local copy of the test system.
+    mols = ala_mols
+
+    # Extract the QM atom selection.
+    qm_atoms = mols[selection].atoms()
+
+    # Create the mapping between molecule numbers and QM atoms.
+    qm_mol_to_atoms = _create_qm_mol_to_atoms(qm_atoms)
+
+    # Get link atom information.
+    mm1_to_qm, mm1_to_mm2, bond_scale_factors = _get_link_atoms(
+        mols, qm_mol_to_atoms, _create_map({})
+    )
+
+    assert mm1_to_qm == expected[0]
+    assert mm1_to_mm2 == expected[1]
+    assert bond_scale_factors == expected[2]
+
+
 @pytest.mark.skipif(not has_emle, reason="emle-engine is not installed")
 @pytest.mark.parametrize("selection", ["molidx 0", "resname ALA"])
 def test_interpolate(ala_mols, selection):
