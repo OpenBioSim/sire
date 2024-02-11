281b56d82ca7456bf75e1ac25c93e580be24e0a3
finlayclark <s1629668@ed.ac.uk>
2024-02-11 12:35:13 +0000

    Add Boresch analytical correction and test

--- src/sire/restraints/__init__.py
+++ src/sire/restraints/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["positional", "bond", "distance", "boresch"]
+__all__ = ["positional", "bond", "distance", "boresch", "get_standard_state_correction"]
 
-from ._restraints import positional, bond, distance, boresch
+from ._restraints import bond, boresch, distance, positional
+from ._standard_state_correction import get_standard_state_correction
