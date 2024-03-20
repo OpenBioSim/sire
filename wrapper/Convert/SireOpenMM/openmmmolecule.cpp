c545fc2e6de5b18622e9741c70f02499e5577f35
Christopher Woods <chryswoods@hey.com>
2024-03-20 00:49:56 +0000

    Added in the fix for non-reversible ghost bond/angle constraint detection

--- wrapper/Convert/SireOpenMM/openmmmolecule.cpp
+++ wrapper/Convert/SireOpenMM/openmmmolecule.cpp
@@ -736,8 +736,26 @@ void OpenMMMolecule::constructFromAmber(const Molecule &mol,
         dynamic_constraints = map["dynamic_constraints"].value().asABoolean();
     }
 
-    for (auto it = params.bonds().constBegin();
-         it != params.bonds().constEnd();
+    auto bonds = params.bonds();
+
+    if (is_perturbable)
+    {
+        // add in any bonds that exist only in the other state
+        // - use the same r0 but set k to 0
+        for (auto it = params1.bonds().constBegin();
+             it != params1.bonds().constEnd();
+             ++it)
+        {
+            if (not bonds.contains(it.key()))
+            {
+                bonds.insert(it.key(), QPair<AmberBond, bool>(AmberBond(0.0, it.value().first.r0()),
+                                                              it.value().second));
+            }
+        }
+    }
+
+    for (auto it = bonds.constBegin();
+         it != bonds.constEnd();
          ++it)
     {
         const auto bondid = it.key().map(molinfo);
@@ -771,7 +789,7 @@ void OpenMMMolecule::constructFromAmber(const Molecule &mol,
         bool bond_is_not_constrained = true;
 
         if ((not has_massless_atom) and ((this_constraint_type & CONSTRAIN_BONDS) or
-           (has_light_atom and (this_constraint_type & CONSTRAIN_HBONDS))))
+                                         (has_light_atom and (this_constraint_type & CONSTRAIN_HBONDS))))
         {
             bool should_constrain_bond = true;
 
@@ -784,6 +802,14 @@ void OpenMMMolecule::constructFromAmber(const Molecule &mol,
 
                 double k_1 = bondparam1.k() * bond_k_to_openmm;
                 r0_1 = bondparam1.r0() * bond_r0_to_openmm;
+
+                if (r0_1 == 0)
+                {
+                    // we cannot shrink the bond to 0 - this must be
+                    // a bond that is disappearing - we should simply
+                    // keep it the same length
+                    r0_1 = r0;
+                }
 
                 if (std::abs(k_1 - k) > 1e-3 or std::abs(r0_1 - r0) > 1e-3)
                 {
@@ -836,8 +862,26 @@ void OpenMMMolecule::constructFromAmber(const Molecule &mol,
 
     ang_params.clear();
 
-    for (auto it = params.angles().constBegin();
-         it != params.angles().constEnd();
+    auto angles = params.angles();
+
+    if (is_perturbable)
+    {
+        // add in any angles that exist only in the other state
+        // - use the same theta0 but set k to 0
+        for (auto it = params1.angles().constBegin();
+             it != params1.angles().constEnd();
+             ++it)
+        {
+            if (not angles.contains(it.key()))
+            {
+                angles.insert(it.key(), QPair<AmberAngle, bool>(AmberAngle(0.0, it.value().first.theta0()),
+                                                                it.value().second));
+            }
+        }
+    }
+
+    for (auto it = angles.constBegin();
+         it != angles.constEnd();
          ++it)
     {
         const auto angid = it.key().map(molinfo);
@@ -882,6 +926,14 @@ void OpenMMMolecule::constructFromAmber(const Molecule &mol,
 
                     double k_1 = angparam.k() * angle_k_to_openmm;
                     theta0_1 = angparam.theta0();
+
+                    if (theta0_1 == 0)
+                    {
+                        // we cannot shrink the angle to 0 - this must be
+                        // an angle that is disappearing - we should simply
+                        // keep it the same angle
+                        theta0_1 = theta0;
+                    }
 
                     if (std::abs(k_1 - k) > 1e-3 or std::abs(theta0_1 - theta0) > 1e-3)
                     {
