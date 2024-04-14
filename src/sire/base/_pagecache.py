c7f7b9ebe496d005d6b79a1d96f861f8b44fb7dc
Christopher Woods <chryswoods@hey.com>
2024-04-14 18:26:36 +0100

    Got the machinery in place to cache and fetch from Python

---
+++ src/sire/base/_pagecache.py
@@ -0,0 +1,49 @@
+__all__ = ["PageCache"]
+
+from ..legacy.Base import PageCache
+
+
+def __cache__(obj, data):
+    """
+    Add the passed object onto the cache. This will convert the object
+    into a binary form (pickled, then hex-encoded) and it will store
+    it in the cache. This returns a handle to the object in the cache,
+    which can be used to restore it.
+    """
+    from ..legacy.Qt import QByteArray
+    from pickle import dumps
+
+    data = dumps(data)
+
+    # now convert this to a QByteArray
+    b = QByteArray(data.hex())
+    return obj.__orig__cache__(b)
+
+
+def __fetch__(obj):
+    """
+    Fetch the object from the cache and return it
+    """
+    from ..legacy.Qt import QByteArray
+    from pickle import loads
+
+    data = obj.__orig__fetch__()
+
+    data = QByteArray.fromHex(obj)
+    return loads(data)
+
+
+if not hasattr(PageCache, "__orig__cache__"):
+    PageCache.__orig__cache__ = PageCache.cache
+    PageCache.cache = __cache__
+
+    if hasattr(PageCache, "handle"):
+        PageCache.Handle = PageCache.handle
+        delattr(PageCache, "handle")
+
+    if hasattr(PageCache, "page"):
+        PageCache.Page = PageCache.page
+        delattr(PageCache, "page")
+
+    PageCache.Handle.__orig__fetch__ = PageCache.Handle.fetch
+    PageCache.Handle.fetch = __fetch__
