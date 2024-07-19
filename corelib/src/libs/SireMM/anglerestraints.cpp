a500bc9eb5a04447c7e631685a6c26a1e70a2d98
Audrius Kalpokas <akalpokas16@gmail.com>
2024-07-19 18:13:32 +0100

    Add the missing corelib files for angle restraints

---
+++ corelib/src/libs/SireMM/anglerestraints.cpp
@@ -0,0 +1,496 @@
+/********************************************\
+  *
+  *  Sire - Molecular Simulation Framework
+  *
+  *  Copyright (C) 2009  Christopher Woods
+  *
+  *  This program is free software; you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation; either version 3 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program; if not, write to the Free Software
+  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  *  For full details of the license please see the COPYING file
+  *  that should have come with this distribution.
+  *
+  *  You can contact the authors at https://sire.openbiosim.org
+  *
+\*********************************************/
+
+#include "anglerestraints.h"
+
+#include "SireID/index.h"
+
+#include "SireUnits/units.h"
+
+#include "SireStream/datastream.h"
+#include "SireStream/shareddatastream.h"
+
+#include "SireCAS/errors.h"
+
+#include <QDebug>
+
+using namespace SireMM;
+using namespace SireID;
+using namespace SireBase;
+using namespace SireMaths;
+using namespace SireStream;
+using namespace SireUnits;
+using namespace SireUnits::Dimension;
+
+////////////
+//////////// Implementation of AngleRestraint
+////////////
+
+static const RegisterMetaType<AngleRestraint> r_angrest;
+
+/** Serialise to a binary datastream */
+
+QDataStream &operator<<(QDataStream &ds, const AngleRestraint &angrest)
+{
+    writeHeader(ds, r_angrest, 1);
+
+    SharedDataStream sds(ds);
+
+    sds << angrest.atms << angrest._theta0 << angrest._ktheta;
+
+    return ds;
+}
+
+/** Extract from a binary datastream */
+QDataStream &operator>>(QDataStream &ds, AngleRestraint &angrest)
+{
+    VersionID v = readHeader(ds, r_angrest);
+
+    if (v == 1)
+    {
+        SharedDataStream sds(ds);
+
+        sds >> angrest.atms >> angrest._theta0 >> angrest._ktheta;
+    }
+    else
+        throw version_error(v, "1", r_angrest, CODELOC);
+
+    return ds;
+}
+
+/** Null constructor */
+AngleRestraint::AngleRestraint()
+    : ConcreteProperty<AngleRestraint, Property>(),
+      _ktheta(0), _theta0(0)
+{
+}
+
+/** Construct a restraint that acts on the angle within the
+    three atoms 'atom0', 'atom1' and 'atom2' (theta == a(012)),
+    restraining the angle within these atoms */
+AngleRestraint::AngleRestraint(const QList<qint64> &atoms,
+                               const SireUnits::Dimension::Angle &theta0,
+                               const SireUnits::Dimension::HarmonicAngleConstant &ktheta)
+    : ConcreteProperty<AngleRestraint, Property>(),
+      _theta0(theta0), _ktheta(ktheta)
+{
+    // Need to think here about validating the angle and force constant values
+    // if (atoms.count() != 3)
+    // {
+    //     throw SireError::invalid_arg(QObject::tr(
+    //                                      "Wrong number of inputs for an Angle restraint. You need to "
+    //                                      "provide 3 atoms (%1).")
+    //                                      .arg(atoms.count()),
+    //                                  //  .arg(theta0.count())
+    //                                  //  .arg(ktheta.count()),
+    //                                  CODELOC);
+    // }
+
+    // make sure that we have 3 distinct atoms
+    QSet<qint64> distinct;
+    distinct.reserve(3);
+
+    for (const auto &atom : atoms)
+    {
+        if (atom >= 0)
+            distinct.insert(atom);
+    }
+
+    // if (distinct.count() != 3)
+    //     throw SireError::invalid_arg(QObject::tr(
+    //                                      "There is something wrong with the atoms provided. "
+    //                                      "They should all be unique and all greater than or equal to 0."),
+    //                                  CODELOC);
+
+    atms = atoms.toVector();
+}
+
+/* Copy constructor*/
+AngleRestraint::AngleRestraint(const AngleRestraint &other)
+    : ConcreteProperty<AngleRestraint, Property>(other),
+      atms(other.atms), _theta0(other._theta0), _ktheta(other._ktheta)
+
+{
+}
+
+/* Destructor */
+AngleRestraint::~AngleRestraint()
+{
+}
+
+AngleRestraint &AngleRestraint::operator=(const AngleRestraint &other)
+{
+    if (this != &other)
+    {
+        Property::operator=(other);
+        atms = other.atms;
+        _theta0 = other._theta0;
+        _ktheta = other._ktheta;
+    }
+
+    return *this;
+}
+
+bool AngleRestraint::operator==(const AngleRestraint &other) const
+{
+    return atms == other.atms and
+           _theta0 == other._theta0 and
+           _ktheta == other._ktheta;
+}
+
+bool AngleRestraint::operator!=(const AngleRestraint &other) const
+{
+    return not operator==(other);
+}
+
+AngleRestraints AngleRestraint::operator+(const AngleRestraint &other) const
+{
+    return AngleRestraints(*this) + other;
+}
+
+AngleRestraints AngleRestraint::operator+(const AngleRestraints &other) const
+{
+    return AngleRestraints(*this) + other;
+}
+
+const char *AngleRestraint::typeName()
+{
+    return QMetaType::typeName(qMetaTypeId<AngleRestraint>());
+}
+
+const char *AngleRestraint::what() const
+{
+    return AngleRestraint::typeName();
+}
+
+AngleRestraint *AngleRestraint::clone() const
+{
+    return new AngleRestraint(*this);
+}
+
+bool AngleRestraint::isNull() const
+{
+    return atms.isEmpty();
+}
+
+QString AngleRestraint::toString() const
+{
+    if (this->isNull())
+        return QObject::tr("AngleRestraint::null");
+    else
+    {
+        QStringList a;
+
+        for (const auto &atom : atms)
+        {
+            a.append(QString::number(atom));
+        }
+        return QString("AngleRestraint( [%1], theta0=%2, ktheta=%3 )")
+            .arg(a.join(", "))
+            .arg(_theta0.toString())
+            .arg(_ktheta.toString());
+    }
+}
+
+/** Return the force constant for the restraint */
+SireUnits::Dimension::HarmonicAngleConstant AngleRestraint::ktheta() const
+{
+    return this->_ktheta;
+}
+
+/** Return the equilibrium angle for the restraint */
+SireUnits::Dimension::Angle AngleRestraint::theta0() const
+{
+    return this->_theta0;
+}
+
+/** Return the atoms involved in the restraint */
+QVector<qint64> AngleRestraint::atoms() const
+{
+    return this->atms;
+}
+
+///////
+/////// Implementation of AngleRestraints
+///////
+
+/** Serialise to a binary datastream */
+
+static const RegisterMetaType<AngleRestraints> r_angrests;
+
+QDataStream &operator<<(QDataStream &ds, const AngleRestraints &angrests)
+{
+    writeHeader(ds, r_angrests, 1);
+
+    SharedDataStream sds(ds);
+
+    sds << angrests.r
+        << static_cast<const Restraints &>(angrests);
+
+    return ds;
+}
+
+/** Extract from a binary datastream */
+QDataStream &operator>>(QDataStream &ds, AngleRestraints &angrests)
+{
+    VersionID v = readHeader(ds, r_angrests);
+
+    if (v == 1)
+    {
+        SharedDataStream sds(ds);
+
+        sds >> angrests.r >>
+            static_cast<Restraints &>(angrests);
+    }
+    else
+        throw version_error(v, "1", r_angrests, CODELOC);
+
+    return ds;
+}
+
+/** Null constructor */
+AngleRestraints::AngleRestraints()
+    : ConcreteProperty<AngleRestraints, Restraints>()
+{
+}
+
+AngleRestraints::AngleRestraints(const QString &name)
+    : ConcreteProperty<AngleRestraints, Restraints>(name)
+{
+}
+
+AngleRestraints::AngleRestraints(const AngleRestraint &restraint)
+    : ConcreteProperty<AngleRestraints, Restraints>()
+{
+    if (not restraint.isNull())
+        r.append(restraint);
+}
+
+AngleRestraints::AngleRestraints(const QList<AngleRestraint> &restraints)
+    : ConcreteProperty<AngleRestraints, Restraints>()
+{
+    for (const auto &restraint : restraints)
+    {
+        if (not restraint.isNull())
+            r.append(restraint);
+    }
+}
+
+AngleRestraints::AngleRestraints(const QString &name,
+                                 const AngleRestraint &restraint)
+    : ConcreteProperty<AngleRestraints, Restraints>(name)
+{
+    if (not restraint.isNull())
+        r.append(restraint);
+}
+
+AngleRestraints::AngleRestraints(const QString &name,
+                                 const QList<AngleRestraint> &restraints)
+    : ConcreteProperty<AngleRestraints, Restraints>(name)
+{
+    for (const auto &restraint : restraints)
+    {
+        if (not restraint.isNull())
+            r.append(restraint);
+    }
+}
+
+AngleRestraints::AngleRestraints(const AngleRestraints &other)
+    : ConcreteProperty<AngleRestraints, Restraints>(other), r(other.r)
+{
+}
+
+/* Desctructor */
+AngleRestraints::~AngleRestraints()
+{
+}
+
+AngleRestraints &AngleRestraints::operator=(const AngleRestraints &other)
+{
+    if (this != &other)
+    {
+        Restraints::operator=(other);
+        r = other.r;
+    }
+
+    return *this;
+}
+
+bool AngleRestraints::operator==(const AngleRestraints &other) const
+{
+    return r == other.r and Restraints::operator==(other);
+}
+
+bool AngleRestraints::operator!=(const AngleRestraints &other) const
+{
+    return not operator==(other);
+}
+
+const char *AngleRestraints::typeName()
+{
+    return QMetaType::typeName(qMetaTypeId<AngleRestraints>());
+}
+
+const char *AngleRestraints::what() const
+{
+    return AngleRestraints::typeName();
+}
+
+AngleRestraints *AngleRestraints::clone() const
+{
+    return new AngleRestraints(*this);
+}
+
+QString AngleRestraints::toString() const
+{
+    if (this->isEmpty())
+        return QObject::tr("AngleRestraints::null");
+
+    QStringList parts;
+
+    const auto n = this->count();
+
+    if (n <= 10)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            parts.append(QObject::tr("%1: %2").arg(i).arg(this->r.at(i).toString()));
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            parts.append(QObject::tr("%1: %2").arg(i).arg(this->r.at(i).toString()));
+        }
+
+        parts.append("...");
+
+        for (int i = n - 5; i < n; i++)
+        {
+            parts.append(QObject::tr("%1: %2").arg(i).arg(this->r.at(i).toString()));
+        }
+    }
+
+    return QObject::tr("AngleRestraints( name=%1, size=%2\n%3\n )")
+        .arg(this->name())
+        .arg(n)
+        .arg(parts.join("\n"));
+}
+
+/** Return whether or not this is empty */
+bool AngleRestraints::isEmpty() const
+{
+    return this->r.isEmpty();
+}
+
+/** Return whether or not this is empty */
+bool AngleRestraints::isNull() const
+{
+    return this->isEmpty();
+}
+
+/** Return the number of restraints */
+int AngleRestraints::nRestraints() const
+{
+    return this->r.count();
+}
+
+/** Return the number of restraints */
+int AngleRestraints::count() const
+{
+    return this->nRestraints();
+}
+
+/** Return the number of restraints */
+int AngleRestraints::size() const
+{
+    return this->nRestraints();
+}
+
+/** Return the ith restraint */
+const AngleRestraint &AngleRestraints::at(int i) const
+{
+    i = SireID::Index(i).map(this->r.count());
+
+    return this->r.at(i);
+}
+
+/** Return the ith restraint */
+const AngleRestraint &AngleRestraints::operator[](int i) const
+{
+    return this->at(i);
+}
+
+/** Return all of the restraints */
+QList<AngleRestraint> AngleRestraints::restraints() const
+{
+    return this->r;
+}
+
+/** Add a restraints onto the list */
+void AngleRestraints::add(const AngleRestraint &restraint)
+{
+    if (not restraint.isNull())
+        r.append(restraint);
+}
+
+/** Add a restraint onto the list */
+void AngleRestraints::add(const AngleRestraints &restraints)
+{
+    this->r += restraints.r;
+}
+
+/** Add a restraint onto the list */
+AngleRestraints &AngleRestraints::operator+=(const AngleRestraint &restraint)
+{
+    this->add(restraint);
+    return *this;
+}
+
+/** Add a restraint onto the list */
+AngleRestraints AngleRestraints::operator+(const AngleRestraint &restraint) const
+{
+    AngleRestraints ret(*this);
+    ret += restraint;
+    return *this;
+}
+
+/** Add restraints onto the list */
+AngleRestraints &AngleRestraints::operator+=(const AngleRestraints &restraints)
+{
+    this->add(restraints);
+    return *this;
+}
+
+/** Add restraints onto the list */
+AngleRestraints AngleRestraints::operator+(const AngleRestraints &restraints) const
+{
+    AngleRestraints ret(*this);
+    ret += restraints;
+    return *this;
+}
---
+++ corelib/src/libs/SireMM/anglerestraints.h
@@ -0,0 +1,177 @@
+/********************************************\
+  *
+  *  Sire - Molecular Simulation Framework
+  *
+  *  Copyright (C) 2009  Christopher Woods
+  *
+  *  This program is free software; you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation; either version 3 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program; if not, write to the Free Software
+  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  *  For full details of the license please see the COPYING file
+  *  that should have come with this distribution.
+  *
+  *  You can contact the authors at https://sire.openbiosim.org
+  *
+\*********************************************/
+
+#ifndef SIREMM_ANGLERESTRAINTS_H
+#define SIREMM_ANGLERESTRAINTS_H
+
+
+#include "restraints.h"
+#include "SireUnits/dimensions.h"
+#include "SireUnits/generalunit.h"
+
+SIRE_BEGIN_HEADER
+
+namespace SireMM
+{
+    class AngleRestraint;
+    class AngleRestraints;
+}
+
+SIREMM_EXPORT QDataStream &operator<<(QDataStream &, const SireMM::AngleRestraint &);
+SIREMM_EXPORT QDataStream &operator>>(QDataStream &, SireMM::AngleRestraint &);
+
+SIREMM_EXPORT QDataStream &operator<<(QDataStream &, const SireMM::AngleRestraints &);
+SIREMM_EXPORT QDataStream &operator>>(QDataStream &, SireMM::AngleRestraints &);
+
+namespace SireMM
+{
+
+    /** This class represents a single angle restraint between any three
+     *  atoms in a system
+     * @author Christopher Woods
+     */
+    class SIREMM_EXPORT AngleRestraint
+        : public SireBase::ConcreteProperty<AngleRestraint, SireBase::Property>
+    {
+
+        friend SIREMM_EXPORT QDataStream & ::operator<<(QDataStream &, const SireMM::AngleRestraint &);
+        friend SIREMM_EXPORT QDataStream & ::operator>>(QDataStream &, SireMM::AngleRestraint &);
+
+    public:
+        AngleRestraint();
+        AngleRestraint(const QList<qint64> &atoms,
+                       const SireUnits::Dimension::Angle &theta0,
+                       const SireUnits::Dimension::HarmonicAngleConstant &ktheta);
+
+        AngleRestraint(const AngleRestraint &other);
+
+        ~AngleRestraint();
+
+        AngleRestraint &operator=(const AngleRestraint &other);
+
+        bool operator==(const AngleRestraint &other) const;
+        bool operator!=(const AngleRestraint &other) const;
+
+        AngleRestraints operator+(const AngleRestraint &other) const;
+        AngleRestraints operator+(const AngleRestraints &other) const;
+
+        static const char *typeName();
+        const char *what() const;
+
+        AngleRestraint *clone() const;
+
+        QString toString() const;
+
+        bool isNull() const;
+
+        QVector<qint64> atoms() const;
+
+        SireUnits::Dimension::Angle theta0() const;
+        SireUnits::Dimension::HarmonicAngleConstant ktheta() const;
+
+    private:
+        /** Atoms involved in the angle restraint */
+        QVector<qint64> atms;
+
+        /** Equilibrium angle */
+        SireUnits::Dimension::Angle _theta0;
+
+        /** Harmonic angle constant */
+        SireUnits::Dimension::HarmonicAngleConstant _ktheta;
+    };
+
+    /** This class represents a collection of angle restraints */
+    class SIREMM_EXPORT AngleRestraints
+        : public SireBase::ConcreteProperty<AngleRestraints, Restraints>
+    {
+        friend SIREMM_EXPORT QDataStream & ::operator<<(QDataStream &, const SireMM::AngleRestraints &);
+        friend SIREMM_EXPORT QDataStream & ::operator>>(QDataStream &, SireMM::AngleRestraints &);
+
+    public:
+        AngleRestraints();
+        AngleRestraints(const QString &name);
+
+        AngleRestraints(const AngleRestraint &restraint);
+        AngleRestraints(const QList<AngleRestraint> &restraints);
+
+        AngleRestraints(const QString &name,
+                        const AngleRestraint &restraint);
+
+        AngleRestraints(const QString &name,
+                        const QList<AngleRestraint> &restraints);
+
+        AngleRestraints(const AngleRestraints &other);
+
+        ~AngleRestraints();
+
+        AngleRestraints &operator=(const AngleRestraints &other);
+
+        bool operator==(const AngleRestraints &other) const;
+        bool operator!=(const AngleRestraints &other) const;
+
+        static const char *typeName();
+        const char *what() const;
+
+        AngleRestraints *clone() const;
+
+        QString toString() const;
+
+        bool isEmpty() const;
+        bool isNull() const;
+
+        int count() const;
+        int size() const;
+        int nRestraints() const;
+
+        const AngleRestraint &at(int i) const;
+        const AngleRestraint &operator[](int i) const;
+
+        QList<AngleRestraint> restraints() const;
+
+        void add(const AngleRestraint &restraint);
+        void add(const AngleRestraints &restraints);
+
+        AngleRestraints &operator+=(const AngleRestraint &restraint);
+        AngleRestraints &operator+=(const AngleRestraints &restraints);
+
+        AngleRestraints operator+(const AngleRestraint &restraint) const;
+        AngleRestraints operator+(const AngleRestraints &restraints) const;
+
+    private:
+        /** List of restraints */
+        QList<AngleRestraint> r;
+    };
+}
+
+Q_DECLARE_METATYPE(SireMM::AngleRestraint)
+Q_DECLARE_METATYPE(SireMM::AngleRestraints)
+
+SIRE_EXPOSE_CLASS(SireMM::AngleRestraint)
+SIRE_EXPOSE_CLASS(SireMM::AngleRestraints)
+SIRE_END_HEADER
+
+#endif
