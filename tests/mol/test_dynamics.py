70b5c68b5480d8a7899cf86f07de095900b09c3a
Lester Hedges <lester.hedges@gmail.com>
2025-01-14 11:27:50 +0000

    Add test for dynamics save frequency modifications. [ref #256]

--- tests/mol/test_dynamics.py
+++ tests/mol/test_dynamics.py
@@ -75,3 +75,45 @@ def test_cutoff_options(ala_mols):
 
     assert d.info().cutoff() == sr.u("7.5 A")
     assert d.info().cutoff_type() == "PME"
+
+
+@pytest.mark.skipif(
+    "openmm" not in sr.convert.supported_formats(),
+    reason="openmm support is not available",
+)
+def test_sample_frequency(ala_mols):
+    """
+    Test that energies and frames are saved at the correct frequency.
+    """
+
+    from sire.base import ProgressBar
+
+    ProgressBar.set_silent()
+
+    mols = ala_mols
+
+    d = mols.dynamics(platform="Reference", timestep="1 fs")
+
+    # Create a list of lambda windows.
+    lambdas = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+
+    # Run 10 cycles of dynamics, saving energies every 2 fs and frames every 5 fs.
+    for i in range(10):
+        d.run(
+            "1 fs",
+            energy_frequency="2 fs",
+            frame_frequency="5 fs",
+            lambda_windows=lambdas,
+        )
+
+    # Get the energy trajectory.
+    nrg_traj = d.energy_trajectory()
+
+    # Make sure the trajectory has 5 frames.
+    assert len(nrg_traj) == 5
+
+    # Get the updated system.
+    new_mols = d.commit()
+
+    # Check that the trajectory has 2 frames.
+    assert len(new_mols.trajectory()) == 2
