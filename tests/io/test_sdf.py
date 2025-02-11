5fec41772f3d0d78ec1ec72bab75d0b0e5a3f34a
Lester Hedges <lester.hedges@gmail.com>
2025-02-11 13:24:39 +0000

    Add unit test for setting and writing of positive formal charge.

--- tests/io/test_sdf.py
+++ tests/io/test_sdf.py
@@ -6,3 +6,27 @@ def test_cis_or_trans():
     Make sure that we can read an SDF file containing a cis or trans double bond.
     """
     mols = sr.load_test_files("cis_trans_double_bond.sdf")
+
+
+def test_positive_charge():
+    """
+    Make sure that we correctly set postive atomic formal charges on
+    read, then re-convert to the correct SDF format on write.
+    """
+    mol = sr.load_test_files("positive_charge.sdf")[0]
+
+    from math import isclose
+
+    import tempfile
+
+    # Make sure that the charge is set correctly.
+    # A value of 3 in the file should be converted to a charge of 1.
+    assert isclose(mol.charge().value(), 1.0)
+
+    # Write back to file.
+    with tempfile.NamedTemporaryFile(suffix=".sdf") as f:
+        sr.save(mol, f.name)
+
+        # Read back in and check that the charge is still 1.
+        mol = sr.load(f.name)[0]
+        assert isclose(mol.charge().value(), 1.0)
