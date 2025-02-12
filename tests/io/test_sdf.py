7ac6ed2031c39bc537ee7a8c6f95e433cfc39aff
Lester Hedges <lester.hedges@gmail.com>
2025-02-12 09:52:56 +0000

    Test parsing of all SDF charge values.

--- tests/io/test_sdf.py
+++ tests/io/test_sdf.py
@@ -8,25 +8,36 @@ def test_cis_or_trans():
     mols = sr.load_test_files("cis_trans_double_bond.sdf")
 
 
-def test_positive_charge():
+def test_charge():
     """
-    Make sure that we correctly set postive atomic formal charges on
-    read, then re-convert to the correct SDF format on write.
+    Make sure that the SDF formal charge field is parsed correctly.
     """
-    mol = sr.load_test_files("positive_charge.sdf")[0]
+    mol = sr.load_test_files("charge.sdf")[0]
 
     from math import isclose
 
     import tempfile
 
-    # Make sure that the charge is set correctly.
-    # A value of 3 in the file should be converted to a charge of 1.
-    assert isclose(mol.charge().value(), 1.0)
+    # SDF format has a weird mapping for formal charge. Here the keys are
+    # the SDF value, and the values are the formal charge.
+    mapping = {
+        7: -3,
+        6: -2,
+        5: -1,
+        0: 0,
+        3: 1,
+        2: 2,
+        1: 3,
+    }
+
+    # Make sure the charges are parsed correctly.
+    for c0, c1 in zip(mol.property("formal_charge").to_list(), mapping.values()):
+        assert isclose(c0.value(), c1)
 
     # Write back to file.
     with tempfile.NamedTemporaryFile(suffix=".sdf") as f:
         sr.save(mol, f.name)
 
-        # Read back in and check that the charge is still 1.
-        mol = sr.load(f.name)[0]
-        assert isclose(mol.charge().value(), 1.0)
+        # Read back in and check that the charges are still correct.
+        for c0, c1 in zip(mol.property("formal_charge").to_list(), mapping.values()):
+            assert isclose(c0.value(), c1)
