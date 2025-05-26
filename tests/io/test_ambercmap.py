e76ba4a607a503980cdf8f4d7fe8a9dca6efe8e6
Christopher Woods <chryswoods@hey.com>
2025-05-26 16:09:40 +0700

    Added some round-robin tests. The amber test fails because this does have different cmap parameters for the same set of atom types

--- tests/io/test_ambercmap.py
+++ tests/io/test_ambercmap.py
@@ -55,3 +55,67 @@ def test_amber_cmap(tmpdir, amber_cmap):
     with open(f[0], "r") as f1:
         with open(f2[0], "r") as f2:
             assert f1.read() == f2.read()
+
+
+def test_amber_cmap_grotop(tmpdir, amber_cmap):
+    """Testing reading and writing from amber to gromacs and back to amber."""
+    mols = amber_cmap.clone()
+
+    dir = tmpdir.mkdir("test_amber_cmap_grotop")
+
+    # Save to a temporary file in GroTop format.
+    f = sr.save(mols, dir.join("output"), format="GroTop")
+
+    # Load the saved file.
+    mols2 = sr.load(f, show_warnings=False)
+
+    # Save back to prmtop format.
+    f2 = sr.save(mols2, dir.join("output"), format="prmtop")
+
+    # Load the saved file again.
+    mols3 = sr.load(f2, show_warnings=False)
+
+    # check that all of the cmap parameters are the same
+    cmaps = mols[0].property("cmap").parameters()
+    cmaps2 = mols2[0].property("cmap").parameters()
+    cmaps3 = mols3[0].property("cmap").parameters()
+
+    for cmap in cmaps:
+        # find the same cmap in the other two molecules
+        found = False
+
+        for cmap2 in cmaps2:
+            if (
+                cmap.atom0() == cmap2.atom0()
+                and cmap.atom1() == cmap2.atom1()
+                and cmap.atom2() == cmap2.atom2()
+                and cmap.atom3() == cmap2.atom3()
+                and cmap.atom4() == cmap2.atom4()
+            ):
+                # make sure that the parameter is the same
+                for val, val2 in zip(
+                    cmap.parameter().values(), cmap2.parameter().values()
+                ):
+                    assert val == pytest.approx(val2, 1e-3)
+                found = True
+
+        assert found
+
+        found = False
+
+        for cmap3 in cmaps3:
+            if (
+                cmap.atom0() == cmap3.atom0()
+                and cmap.atom1() == cmap3.atom1()
+                and cmap.atom2() == cmap3.atom2()
+                and cmap.atom3() == cmap3.atom3()
+                and cmap.atom4() == cmap3.atom4()
+            ):
+                # make sure that the parameter is the same
+                for val, val3 in zip(
+                    cmap.parameter().values(), cmap3.parameter().values()
+                ):
+                    assert val == pytest.approx(val3, 1e-3)
+                found = True
+
+        assert found
--- tests/io/test_grotop.py
+++ tests/io/test_grotop.py
@@ -252,3 +252,67 @@ def test_grotop_cmap(tmpdir, gromacs_cmap):
 
         if cmaps is not None:
             assert len(cmaps.parameters()) == 0
+
+
+def test_grotop_cmap_amber(tmpdir, gromacs_cmap):
+    """Testing reading and writing from gromacs to amber and back to gromacs"""
+    mols = gromacs_cmap.clone()
+
+    dir = tmpdir.mkdir("test_grotop_cmap_amber")
+
+    # Save to a temporary file in Amber format.
+    f = sr.save(mols, dir.join("output"), format="prmtop")
+
+    # Load the saved file.
+    mols2 = sr.load(f, show_warnings=False)
+
+    # Save back to GROMACS format.
+    f2 = sr.save(mols2, dir.join("output"), format="GroTop")
+
+    # Load the saved file again.
+    mols3 = sr.load(f2, show_warnings=False)
+
+    # check that all of the cmap parameters are the same
+    cmaps = mols[0].property("cmap").parameters()
+    cmaps2 = mols2[0].property("cmap").parameters()
+    cmaps3 = mols3[0].property("cmap").parameters()
+
+    for cmap in cmaps:
+        # find the same cmap in the other two molecules
+        found = False
+
+        for cmap2 in cmaps2:
+            if (
+                cmap.atom0() == cmap2.atom0()
+                and cmap.atom1() == cmap2.atom1()
+                and cmap.atom2() == cmap2.atom2()
+                and cmap.atom3() == cmap2.atom3()
+                and cmap.atom4() == cmap2.atom4()
+            ):
+                # make sure that the parameter is the same
+                for val, val2 in zip(
+                    cmap.parameter().values(), cmap2.parameter().values()
+                ):
+                    assert val == pytest.approx(val2, 1e-3)
+                found = True
+
+        assert found
+
+        found = False
+
+        for cmap3 in cmaps3:
+            if (
+                cmap.atom0() == cmap3.atom0()
+                and cmap.atom1() == cmap3.atom1()
+                and cmap.atom2() == cmap3.atom2()
+                and cmap.atom3() == cmap3.atom3()
+                and cmap.atom4() == cmap3.atom4()
+            ):
+                # make sure that the parameter is the same
+                for val, val3 in zip(
+                    cmap.parameter().values(), cmap3.parameter().values()
+                ):
+                    assert val == pytest.approx(val3, 1e-3)
+                found = True
+
+        assert found
