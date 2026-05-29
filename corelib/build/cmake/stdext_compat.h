6e2ffa2661ebe124b982927a224e268655b2499e
Lester Hedges <lester.hedges@gmail.com>
2026-05-29 21:46:36 +0100

    Guard stdext polyfill with __cplusplus to fix C file compilation.

--- corelib/build/cmake/stdext_compat.h
+++ corelib/build/cmake/stdext_compat.h
@@ -3,7 +3,7 @@
 // removed in MSVC 14.38 (VS 2022 17.8). Qt5's qcompilerdetection.h maps
 // QT_MAKE_CHECKED_ARRAY_ITERATOR and QT_MAKE_UNCHECKED_ARRAY_ITERATOR to these
 // functions unconditionally on MSVC, so provide no-op stubs.
-#if defined(_MSC_VER) && _MSC_VER >= 1938
+#if defined(__cplusplus) && defined(_MSC_VER) && _MSC_VER >= 1938
 #include <cstddef>
 namespace stdext
 {
